42abf5a9d20d615e8322214549e823c03bc5ce30
rakesh <prorakeshraut@gmail.com>
2018-02-18 19:41:15 +0545

    Python triangle with output

--- triangle.py
+++ triangle.py
@@ -5,6 +5,15 @@ for i in range(5):
     for j in range(i+1):
         print("*", end="")
     print("\n")
+    print('')
 
+   #output
+'''*
+   **
+   ***
+   ****
+   *****'''
 
-    
+    '''print("Asterisk triangle")
+    for g in range(11, 0, -1):
+        print(g * ' ' + (11 - g) * '*')'''
